0f52c6908123804c13befed480976aad5884ed83
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-11-07 22:04:14 +0100

    feat: event listener added for the button in the DOM

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -2,5 +2,9 @@
 console.log('content script')
 //const body = document.body.innerHTML;
 
-document.body.innerHTML += "<h1 id='title' style='background-color:lime; position:fixed; top:0px;'>Some Title</h1>";
+
+document.body.innerHTML += "<h1 id='add' style='background-color:lime; position:fixed; top:0px; z-index:999999'>Some Title</h1>";
+document.getElementById('add').addEventListener('click', function () {
+  console.log('finally');
+})
 //document.body.insertBefore("<h1 id='title' style='z-index=100'>Some Title</h1><span>test</span>", dom.document.html.firstChild)
