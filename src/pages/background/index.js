9fa1bed7b2c610e5bbd453fdafcc9f433adc5931
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-11-07 23:03:08 +0100

    feat: working on communication between content script and background

--- src/pages/background/index.js
+++ src/pages/background/index.js
@@ -1,26 +1,26 @@
 import axios from 'axios';
 import store from './store';
 
-
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    //code in here will run every time a user goes onto a new tab, so you can insert your scripts into every new tab
-
-    // var div=document.createElement("div");
-    // document.body.appendChild(div);
-    // div.innerText="test123";
-    // let x = chrome.tabs.executeScript({
-    //
-    // });
-});
-
 chrome.commands.onCommand.addListener(function(command) {
-
   alert('hello')
 });
 
 chrome.tabs.onActivated.addListener(function(tabs) {
   chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    console.log(tabs[0].url);
+    // chrome.runtime.onMessage.addListener(
+    //   function(request, sender, sendResponse) {
+    //     if (request.message == "url") {
+    //       console.log('url list', tabs)
+    //       sendResponse({farewell: tabs[0].url.toString()});
+    //     }
+    // });
+    chrome.runtime.onConnect.addListener(function(port) {
+      console.assert(port.name == "url-connection");
+      port.onMessage.addListener(function(msg) {
+        if (msg.message == "url")
+          port.postMessage({result: tabs[0].url.toString()});
+      });
+    });
+    console.log('current URL:',tabs[0].url);
   });
 })
--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -1,10 +1,21 @@
-//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Google", "gi"), "goooooooooooooogle");
 console.log('content script')
-//const body = document.body.innerHTML;
 
+document.body.innerHTML += "<img id='add' src='http://www.downloadclipart.net/medium/18122-blue-pin-clip-art.png'style='width: 50px; position:fixed; top:5px; left:5px; z-index:999999'>Some Title />";
 
-document.body.innerHTML += "<h1 id='add' style='background-color:lime; position:fixed; top:0px; z-index:999999'>Some Title</h1>";
+var port = chrome.runtime.connect({name: "url-connection"});
+//Event listener for click to the button add
 document.getElementById('add').addEventListener('click', function () {
   console.log('finally');
+  // I want to ask to the background the url of the tab
+
+  // chrome.runtime.sendMessage({message: "url"}, function(response) {
+  //   console.log('response', response, 'response farewell', response.farewell);
+  // });
+  port.postMessage({message: "url"});
+  port.onMessage.addListener(function(msg) {
+    if (msg.result){
+      console.log('OK', msg.result)
+      port.postMessage({answer: "Madame"});
+      }
+  });
 })
-//document.body.insertBefore("<h1 id='title' style='z-index=100'>Some Title</h1><span>test</span>", dom.document.html.firstChild)
