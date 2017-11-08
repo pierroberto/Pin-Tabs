d5f79a95e92f19805349576519b0162013da872d
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-11-08 09:41:39 +0100

    fix: communication between content script and background

--- src/pages/background/index.js
+++ src/pages/background/index.js
@@ -5,22 +5,13 @@ chrome.commands.onCommand.addListener(function(command) {
   alert('hello')
 });
 
-chrome.tabs.onActivated.addListener(function(tabs) {
-  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    // chrome.runtime.onMessage.addListener(
-    //   function(request, sender, sendResponse) {
-    //     if (request.message == "url") {
-    //       console.log('url list', tabs)
-    //       sendResponse({farewell: tabs[0].url.toString()});
-    //     }
-    // });
-    chrome.runtime.onConnect.addListener(function(port) {
-      console.assert(port.name == "url-connection");
-      port.onMessage.addListener(function(msg) {
-        if (msg.message == "url")
-          port.postMessage({result: tabs[0].url.toString()});
+chrome.runtime.onConnect.addListener(function(port) {
+  console.assert(port.name == "url-connection");
+  port.onMessage.addListener(function(msg) {
+    if (msg.message == "url") {
+      chrome.tabs.getSelected(null,function(tab) {
+        port.postMessage({result: tab.url.toString()});
       });
-    });
-    console.log('current URL:',tabs[0].url);
+    }
   });
-})
+});
