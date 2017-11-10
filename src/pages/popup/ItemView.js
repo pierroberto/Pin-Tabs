c0d0599a5128e344d70623db9fc64700effa1e3a
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-11-10 21:52:21 +0100

    style: align delete buttons to the right

--- src/pages/popup/ItemView.js
+++ src/pages/popup/ItemView.js
@@ -6,9 +6,13 @@ export default class ItemView extends React.Component {
   render () {
     return (
       <div className='content-container'>
+        <div className='col-1'>
           <img className='icon' src={this.props.tab_icon} />
           <div className='title'>{this.props.tab_title}</div>
+        </div>
+        <div className='col-2'>
           <img className='delete-button' src='../assets/delete.png' onClick={()=>this.props.dataCallback(this.props.tab_url)} />
+        </div>
       </div>
     )
   }
