825930f3877358d77dd232e83b90ae7d57686460
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-12-31 17:10:25 +0100

    Feat: add footer + fix youtube bug

--- src/pages/popup/Dashboard.js
+++ src/pages/popup/Dashboard.js
@@ -1,143 +1,158 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import {refreshBookmark, deleteAllBookmark, deleteOneBookmark, addBookmark, addFromButton} from '../background/actions';
-import './dashboard.css';
-import ListView from './ListView.js';
-import Settings from './Settings';
-import Search from './Search'
-import truncate from 'truncate';
-import {Link} from 'react-router-dom'
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  refreshBookmark,
+  deleteAllBookmark,
+  deleteOneBookmark,
+  addBookmark,
+  addFromButton
+} from "../background/actions";
+import "./dashboard.css";
+import ListView from "./ListView.js";
+import Settings from "./Settings";
+import Search from "./Search";
+import truncate from "truncate";
+import { Link } from "react-router-dom";
 
 class Dashboard extends React.Component {
-
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
   }
 
-  saveBookmark () {
-    return new Promise ((resolved, rejected) => {
-      chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (data) => {
+  saveBookmark() {
+    return new Promise((resolved, rejected) => {
+      chrome.tabs.query({ active: true, lastFocusedWindow: true }, data => {
         resolved(data);
-      })
-    })
-    .then (link => {
-      if (!link[0].favIconUrl) link[0].favIconUrl='../assets/nothing.png';
-      if (link[0].title.length > 10) link[0].title = truncate(link[0].title.toString(), 50)
+      });
+    }).then(link => {
+      if (!link[0].favIconUrl) link[0].favIconUrl = "../assets/nothing.png";
+      if (link[0].title.length > 10)
+        link[0].title = truncate(link[0].title.toString(), 50);
       const flag = this.checkUrl(link);
       if (flag) {
-        this.props.add(link)
-      }
-      else {
+        this.props.add(link);
+      } else {
         //If it already exists remove the old link and add a new one
-        this.props.deleteOne(link[0].url)
-        this.props.add(link)
+        this.props.deleteOne(link[0].url);
+        this.props.add(link);
       }
-    })
+    });
   }
 
-  checkUrl (link) {
+  checkUrl(link) {
     for (let i = 0; i < this.props.bookmark.tabs.length; i++) {
       if (this.props.bookmark.tabs[i].tab[0].url === link[0].url) return false;
     }
     return true;
   }
 
-  clearAll () {
-    this.props.deleteAll()
+  clearAll() {
+    this.props.deleteAll();
   }
 
-  deleteTab = (url) => {
-    this.props.deleteOne(url)
+  deleteTab = url => {
+    this.props.deleteOne(url);
     return data;
-  }
+  };
 
   componentDidMount() {
     // VERSION 1.0.2 Double check if the link has expireDate
 
-    this.props.bookmark.tabs.map ((tab) => {
-      console.log('tab', tab.tab[0].url);
-      if (tab.expiry >= (this.props.settings.expireDate + Date.now())) {
-        console.log('inside if');
-        store.dispatch({type:'EXPIRY', url: tab.tab[0].url})
+    this.props.bookmark.tabs.map(tab => {
+      console.log("tab", tab.tab[0].url);
+      if (tab.expiry >= this.props.settings.expireDate + Date.now()) {
+        console.log("inside if");
+        store.dispatch({ type: "EXPIRY", url: tab.tab[0].url });
       }
-    })
+    });
   }
 
-  checkSearch () {
+  checkSearch() {
     if (!this.props.bookmark.search) {
       return (
         <ListView
           tabs={this.props.bookmark.tabs}
           deleteTab={this.deleteTab}
-          action='renderBookmark'
+          action="renderBookmark"
           expirySettings={this.props.settings.expireDate}
         />
-      )
+      );
     }
   }
 
-// ====================== RENDERING
+  // ====================== RENDERING
 
-  render () {
+  render() {
     // I check if the button in the content script has been clicked
     if (this.props.bookmark.addFromButton) {
-      this.saveBookmark()
-      this.props.addThroughButton(false)
+      this.saveBookmark();
+      this.props.addThroughButton(false);
     }
 
     return (
-
       <div>
-        <div className='header'>
+        <div className="header">
           <h1>Pin Tabs</h1>
-          <Link to={'/pages/settings'} style={{color:'black'}}>
-            <i class="fa fa-cog fa-2x"></i>
+          <Link to={"/pages/settings"} style={{ color: "black" }}>
+            <i class="fa fa-cog fa-2x" />
           </Link>
         </div>
 
-        <div className='nav'>
-
-          <div className='col-1 custom'>
+        <div className="nav">
+          <div className="col-1 custom">
             <button onClick={() => this.saveBookmark()}>Add</button>
             {/* <button onClick={() => this.clearAll()}>Delete All</button> */}
             <Search />
           </div>
 
-
-          <div className='col-2'>
-
-          </div>
+          <div className="col-2" />
         </div>
 
         {this.checkSearch()}
 
-        <div className={this.props.settings.buttonHistory ? 'visible':'hidden'}>
-          <h2 className='history'>History</h2>
+        <div
+          className={this.props.settings.buttonHistory ? "visible" : "hidden"}
+        >
+          <h2 className="history">History</h2>
           <ListView
             tabs={this.props.bookmark.chronology}
             deleteTab={false}
-            action='renderChronology'
+            action="renderChronology"
             expired={true}
           />
         </div>
+        <div className="footer">
+          <div className="footer__author">Pier Roberto Lucisano 📌 2018</div>
+          <div className="footer__social">
+            <div className="footer__github">
+              <i class="fa fa-github fa-lg" aria-hidden="true" />
+              <a
+                className="footer__link"
+                href="https://github.com/pierroberto/Pin-Tabs"
+                target="_blank"
+              >
+                Github
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
-  bookmark : state.bookmark,
-  settings : state.settings,
+const mapStateToProps = state => ({
+  bookmark: state.bookmark,
+  settings: state.settings,
   animation: state.animation
 });
 
-const mapDispatchToProps  = (dispatch) => ({
-  add: (url) => dispatch(addBookmark(url)),
-  refresh: (data) => dispatch(refreshBookmark(data)),
+const mapDispatchToProps = dispatch => ({
+  add: url => dispatch(addBookmark(url)),
+  refresh: data => dispatch(refreshBookmark(data)),
   deleteAll: () => dispatch(deleteAllBookmark()),
-  deleteOne: (url) => dispatch(deleteOneBookmark(url)),
-  addThroughButton : (flag) => dispatch(addFromButton(flag))
+  deleteOne: url => dispatch(deleteOneBookmark(url)),
+  addThroughButton: flag => dispatch(addFromButton(flag))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
--- src/pages/popup/Settings.js
+++ src/pages/popup/Settings.js
@@ -1,24 +1,25 @@
-import {Store} from 'react-chrome-redux';
-import React, { Component } from 'react';
-import './settings.css';
-import './App';
-import {Link} from 'react-router-dom';
-import { connect } from 'react-redux';
-import { toggleButton, toggleButtonHistory, expireDate } from '../background/actions';
-
+import { Store } from "react-chrome-redux";
+import React, { Component } from "react";
+import "./settings.css";
+import "./App";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import {
+  toggleButton,
+  toggleButtonHistory,
+  expireDate
+} from "../background/actions";
 
 class Settings extends React.Component {
-
   // ======================== RENDERING
 
-  render () {
-    console.log('props in actions', this.props);
+  render() {
     return (
       <div>
-        <div className='header'>
+        <div className="header">
           <h1>Settings</h1>
-          <Link to={'/pages/popup.html'} style={{color:'black'}}>
-            <i class="fa fa-check fa-2x"></i>
+          <Link to={"/pages/popup.html"} style={{ color: "black" }}>
+            <i class="fa fa-check fa-2x" />
           </Link>
         </div>
 
@@ -26,45 +27,53 @@ class Settings extends React.Component {
           <ul>
             <li>
               <label>Keep bookmarks for</label>
-                <select value={this.props.settings.expireDate} onChange={(e)=>this.props.expire(parseInt(e.target.value))}>
-                  <option value='10000'>1 sec</option>
-                  <option value='3600000'>1 hour</option>
-                  <option value='43200000'>12 hours</option>
-                  <option value='86400000'>1 day</option>
-                  <option value='172800000'>2 days</option>
-                  <option value='259200000'>3 days</option>
-                  <option value='345600000'>4 days</option>
-                  <option value='432000000'>5 days</option>
-                  <option value='518400000'>6 days</option>
-                  <option value='604800000'>1 week</option>
-                </select>
+              <select
+                value={this.props.settings.expireDate}
+                onChange={e => this.props.expire(parseInt(e.target.value))}
+              >
+                <option value="3600000">1 hour</option>
+                <option value="43200000">12 hours</option>
+                <option value="86400000">1 day</option>
+                <option value="172800000">2 days</option>
+                <option value="259200000">3 days</option>
+                <option value="345600000">4 days</option>
+                <option value="432000000">5 days</option>
+                <option value="518400000">6 days</option>
+                <option value="604800000">1 week</option>
+              </select>
             </li>
             <li>
               <label>Show icon</label>
-                <input type='checkbox' checked={this.props.settings.button} onChange={(e) => this.props.toggle(e.target.checked)}/>
+              <input
+                type="checkbox"
+                checked={this.props.settings.button}
+                onChange={e => this.props.toggle(e.target.checked)}
+              />
             </li>
             <li>
               <label>Show history</label>
-                <input type='checkbox' checked={this.props.settings.buttonHistory} onChange={(e) => this.props.toggleHistory(e.target.checked)}/>
+              <input
+                type="checkbox"
+                checked={this.props.settings.buttonHistory}
+                onChange={e => this.props.toggleHistory(e.target.checked)}
+              />
             </li>
-
           </ul>
         </form>
-
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
-  bookmark : state.bookmark,
-  settings : state.settings
+const mapStateToProps = state => ({
+  bookmark: state.bookmark,
+  settings: state.settings
 });
 
-const mapDispatchToProps  = (dispatch) => ({
-  toggle : (flag) => dispatch(toggleButton(flag)),
-  toggleHistory : (flag) => dispatch(toggleButtonHistory(flag)),
-  expire : (date) => dispatch(expireDate(date))
+const mapDispatchToProps = dispatch => ({
+  toggle: flag => dispatch(toggleButton(flag)),
+  toggleHistory: flag => dispatch(toggleButtonHistory(flag)),
+  expire: date => dispatch(expireDate(date))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Settings);
