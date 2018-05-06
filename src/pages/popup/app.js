99ec9990100cb1f5d64aad27aaeca0f0a77deec1
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-05-06 18:15:01 +0200

    Chore: change description + version of manifest

--- src/pages/popup/app.js
+++ src/pages/popup/app.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './app.css';
-import ListView from './ListView.js';
-import Settings from './Settings';
-import Dashboard from './Dashboard';
-import {Route, BrowserRouter as Router,Switch} from 'react-router-dom'
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./app.css";
+import ListView from "./ListView.js";
+import Settings from "./Settings";
+import Dashboard from "./Dashboard";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 class App extends React.Component {
-
-  render () {
+  render() {
     return (
       <Router>
-        <div className='wrapper'>
+        <div className="wrapper">
           <Switch>
-              <Route exact path='/pages/popup.html' component={Dashboard} />
-              <Route path='/pages/settings' component={Settings} />
+            <Route exact path="/pages/popup.html" component={Dashboard} />
+            <Route path="/pages/settings" component={Settings} />
           </Switch>
-      </div>
+        </div>
       </Router>
-    )
+    );
   }
 }
 
